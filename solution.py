5a148831d315b3eda6feb8235786a5e1909870ba
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-08 00:40:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
+
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 
 
 chat_id = 1134491798
 
 def solution(x: np.array) -> float:
-  
-    n = len(x)
-    mu = x.mean()
-    se = np.sqrt(1/n)
-    t = norm.ppf(0.975)  
-    return t * se / mu
+    y = x / 10  
+    b = np.sum(y) / np.sum(10 - np.mean(x))  
+    return b + 19 
