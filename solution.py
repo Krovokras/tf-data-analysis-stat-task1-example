5bc36aa8e64d2a7bc4037d4b2f7bfcf480085f33
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-08 00:19:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
+
 
 chat_id = 1134491798
 
 def solution(x: np.array) -> float:
-    df = pd.DataFrame({'chat_id': chat_id, 'x': x})
-    group_means = df.groupby('chat_id')['x'].mean()
-    return group_means[chat_id]
+  
+    n = len(x)
+    mu = x.mean()
+    se = np.sqrt(1/n)
+    t = norm.ppf(0.975)  
+    return t * se / mu
