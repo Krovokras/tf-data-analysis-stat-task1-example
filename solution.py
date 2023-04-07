76d28a1906633a7e5d3772a6e097561e283a3375
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-07 21:41:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
+
+
 import pandas as pd
 import numpy as np
 
 
-chat_id = 1134491798 # Ваш chat ID, не меняйте название переменной
+chat_id = 1134491798
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-      return x[x == chat_id].shape[0] / x.shape[0]  # Ваш ответ
+    y = x ** 2 + chat_id
+    return y.mean()
+
+    
